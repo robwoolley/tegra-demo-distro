d71e2d1873ee7aa40529b71e8234fcae30dc9ec4
Rob Woolley <rob.woolley@windriver.com>
2025-07-01 22:26:59 -0400

    Replace bb.debug with bb.note
    This is a temporary measure while trying to get this working
    with the oeros GitLab CI.
    
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- layers/meta-demo-ci/lib/oeaws/s3session.py
+++ layers/meta-demo-ci/lib/oeaws/s3session.py
@@ -43,7 +43,7 @@ class S3Session(object):
         self.s3client = boto3.Session(botocore_session=session).client('s3')
         provider = session.get_component('credential_provider').get_provider('assume-role')
         provider.cache = botocore.credentials.JSONFileCache()
-        bb.debug(1, "Using AWS profile: %s" % provider._profile_name)
+        bb.note("Using AWS profile: %s" % provider._profile_name)
 
     def upload(self, Filename, Bucket, Key):
         if self.s3client is None:
@@ -67,7 +67,7 @@ class S3Session(object):
             self.makeclient()
         for attempt in range(10):
             try:
-                bb.debug(2, "%s/%s: attempt %d" % (Bucket, Key, attempt))
+                bb.note("%s/%s: attempt %d" % (Bucket, Key, attempt))
                 info = self.s3client.head_object(Bucket=Bucket, Key=Key)
                 self.s3client.download_file(Bucket=Bucket, Key=Key, Filename=Filename)
                 if 'LastModified' in info:
@@ -79,7 +79,7 @@ class S3Session(object):
             except botocore.exceptions.ClientError as e:
                 err = e.response['Error']
                 if quiet and err['Code'] == "404":
-                    bb.debug(2, "not found: {}/{}".format(Bucket, Key))
+                    bb.note("not found: {}/{}".format(Bucket, Key))
                 else:
                     bb.warn("{}/{}: {} {}".format(Bucket, Key, err['Code'], err['Message']))
                 return False
@@ -88,7 +88,7 @@ class S3Session(object):
                     pass
                 bb.warn("os.utime({}): {} (errno {})".format(Filename, e.strerror, e.errno))
                 return False
-            bb.debug(1, "{}/{}: success".format(Bucket, Key))
+            bb.note("{}/{}: success".format(Bucket, Key))
             return True
         bb.warn("{}/{}: credentials error on download for 10 attempts".format(Bucket, Key))
         return False
@@ -105,7 +105,7 @@ class S3Session(object):
             except botocore.exceptions.ClientError as e:
                 err = e.response['Error']
                 if quiet and err['Code'] == "404":
-                    bb.debug(2, "not found: {}/{}".format(Bucket, Key))
+                    bb.note("not found: {}/{}".format(Bucket, Key))
                 else:
                     bb.warn("{}/{}: {} {}".format(Bucket, Key, err['Code'], err['Message']))
                 return None
